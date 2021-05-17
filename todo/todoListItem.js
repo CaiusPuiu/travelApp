7ec577ff25e1570c33a1494f3d0c1e296d2c899d
Caius Puiu <caiuspuiu@gmail.com>
2021-05-17 20:42:51 +0300

    session8 webcomponents

--- todo/todoListItem.js
+++ todo/todoListItem.js
@@ -4,12 +4,38 @@ class TodoListItem extends LitElement {
   static get properties() {
     return {
       todoItem: { type: Object },
+      _isEditing: { type: Boolean },
     };
   }
 
   render() {
-    return html` ${this.todoItem.text} `;
+    return html`
+      ${this._isEditing
+        ? html`<form @submit=${this._handleValueChanged}>
+            <input type="text" .value=${this.todoItem.text} name="text" />
+          </form>`
+        : this.todoItem.text}
+      <button @click=${this._handleEditClick}>
+        ${this._isEditing ? 'Save' : 'Edit'}
+      </button>
+    `;
+  }
+
+  _handleEditClick() {
+    this._isEditing = true;
+  }
+
+  _handleValueChanged(e) {
+    e.preventDefault();
+    this._isEditing = false;
+
+    const updateTodoEvent = new CustomEvent('updateTodo', {
+      detail: Object.fromEntries(new FormData(e.target)),
+    });
+
+    this.dispatchEvent(updateTodoEvent);
   }
 }
 
 customElements.define('todo-list-item', TodoListItem);
+
